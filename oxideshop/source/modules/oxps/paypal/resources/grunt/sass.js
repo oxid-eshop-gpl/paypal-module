00a3955e2c9dd74e11df7ec77c6aca4f53d17f4f
Mario Lorenz <mario.lorenz@oxid-esales.com>
2020-07-17 14:23:14 +0200

    feat: add new grunt process "development"

--- oxideshop/source/modules/oxps/paypal/resources/grunt/sass.js
+++ oxideshop/source/modules/oxps/paypal/resources/grunt/sass.js
@@ -1,6 +1,17 @@
 const sass = require('node-sass');
 
 module.exports = {
+    moduledevelopment: {
+        options: {
+            implementation: sass,
+            update: true,
+            style: 'nested'
+        },
+        files: {
+            "../out/src/css/paypal.css": "build/scss/paypal.scss",
+            "../out/src/css/paypal-admin.css": "build/scss/paypal-admin.scss",
+        }
+    },
     moduleproduction: {
         options: {
             implementation: sass,
