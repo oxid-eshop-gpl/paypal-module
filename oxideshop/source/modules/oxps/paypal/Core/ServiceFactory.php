4e4364aaa3bf5a4606c83faca97d753c2700fa49
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-07-27 09:32:38 +0000

    do not use payerid

--- oxideshop/source/modules/oxps/paypal/Core/ServiceFactory.php
+++ oxideshop/source/modules/oxps/paypal/Core/ServiceFactory.php
@@ -56,7 +56,9 @@ class ServiceFactory
                 $config->isSandbox() ? Client::SANDBOX_URL : Client::PRODUCTION_URL,
                 $config->getClientId(),
                 $config->getClientSecret(),
-                'W56FXEVJQ3SWW', // TODO change this to the value from config when available
+                '', // must be empty. We do not have the merchant's payerid
+                //and confirmed by paypal we should not use it for auth and 
+                //so not ask for it on the configuration page
                 false
             );
             $this->client = $client;
