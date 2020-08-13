37df5f6da4508caabfc132f8e4198188e4a2f755
Mario Lorenz <mario.lorenz@oxid-esales.com>
2020-08-13 08:49:58 +0200

    feat: update npm resources

---
+++ oxideshop/source/modules/oxps/paypal/resources/grunt/concat.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    moduleproduction: {
+        options: {
+            seperator: ";"
+        },
+        files: {
+            "../out/src/js/paypal.min.js": "build/js/paypal.js",
+            "../out/src/js/paypal-admin.min.js": "build/js/paypal-admin.js"
+        }
+    }
+};
