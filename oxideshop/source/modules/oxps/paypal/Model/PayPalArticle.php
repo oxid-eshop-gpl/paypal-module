b0b95e9878a5349df42a42e3d60b4fc70bd38a72
Marc Plotz <marc.plotz@oxid-esales.com>
2020-08-24 10:12:37 +0200

    cleanup

--- oxideshop/source/modules/oxps/paypal/Model/PayPalArticle.php
+++ oxideshop/source/modules/oxps/paypal/Model/PayPalArticle.php
@@ -9,28 +9,44 @@ namespace OxidProfessionalServices\PayPal\Model;
  */
 class PayPalArticle extends PayPalArticle_parent
 {
+    /**
+     * @return bool
+     */
     public function isPayPalProductLinked()
     {
         return $this->getPaypalProductId() !== "";
     }
 
+    /**
+     * @return bool
+     */
     public function isThisOrParentPayPalProductLinked()
     {
         return $this->isPayPalProductLinked() || $this->getPaypalParentArticle()->isPayPalProductLinked();
     }
 
+    /**
+     * @return string
+     */
     public function getPaypalProductId(): string
     {
         return (string) $this->getFieldData("PaypalProductId");
     }
 
+    /**
+     * @return bool
+     */
     public function isPayPalProductLinkedByParentOnly()
     {
         $parent = $this->getPaypalParentArticle();
         return (!$this->isPayPalProductLinked()) && $parent->isPayPalProductLinked();
     }
 
-    public function getPaypalParentArticle(): self {
+    /**
+     * @return $this
+     */
+    public function getPaypalParentArticle()
+    {
         $parent = $this->getParentArticle();
         assert($parent instanceof self);
         return $parent;
